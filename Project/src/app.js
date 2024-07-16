a6598eee16f50a030c15c56eb08567c5fd8c96c6
paras29exe <dimplesaini5252@gmail.com>
2024-07-16 22:52:13 +0530

    added likes and subscription controllers

--- Project/src/app.js
+++ Project/src/app.js
@@ -19,12 +19,16 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 // using routes
-// we can't use app.get here because "routers" are in separate files and now we are giving control to user.routes.js
+// we can't use app.get here because "routers" are in separate files and now we are routing in this file
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/likes", likeRouter)
 
 export {app}
--- Project/src/routes/subscription.routes.js
+++ Project/src/routes/subscription.routes.js
@@ -1,7 +1,13 @@
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { toggleSubscription, subscribedChannels, subscribersList } from "../controllers/subscription.controller.js";
 import { Router } from "express";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.route("/toggle-subscription/:channelId").post(verifyJWT, toggleSubscription);
+
+subscriptionRouter.route("/subscribed-channels/current-user").get(verifyJWT, subscribedChannels);
+
+subscriptionRouter.route("/subscribers-list/current-user").get(verifyJWT, subscribersList);
+
+export default subscriptionRouter;
